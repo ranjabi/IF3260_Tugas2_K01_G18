ccef5391d7e699fd6579ab45735ce4aed340eb31
addinnabilal <13520045@std.stei.itb.ac.id>
2023-03-22 16:07:01 +0700

    feat: random hollow object (unfinished)

--- src/classes/object.js
+++ src/classes/object.js
@@ -157,5 +157,172 @@ export default class BaseObject {
     
             return outputPoints
         }
+
+        
+    }
+
+    initHollowPrism() {
+        // Define the vertices of the triangles for the outer prism
+        let outerVertices = [       
+            [-0.5, -0.5, -0.5], // bottom left back
+            [0.5, -0.5, -0.5], // bottom right back
+            [0, 0.5, -0.5], // top back
+            [-0.5, -0.5, 0.5], // bottom left front
+            [0.5, -0.5, 0.5], // bottom right front
+            [0, 0.5, 0.5], // top front
+        ];
+    
+        // Define the vertices of the triangles for the inner prism
+        let innerVertices = [        
+            [-0.3, -0.3, -0.3], // inner bottom left back
+            [0.3, -0.3, -0.3], // inner bottom right back
+            [0, 0.3, -0.3], // inner top back
+            [-0.3, -0.3, 0.3], // inner bottom left front
+            [0.3, -0.3, 0.3], // inner bottom right front
+            [0, 0.3, 0.3], // inner top front
+        ];
+    
+        // Define the colors of each vertex
+        let vertexColors = [        
+            [1.0, 0.0, 0.0, 1.0], // red
+            [1.0, 1.0, 0.0, 1.0], // yellow
+            [0.0, 1.0, 0.0, 1.0], // green
+            [0.0, 0.0, 1.0, 1.0], // blue
+            [1.0, 0.0, 1.0, 1.0], // magenta
+            [0.0, 1.0, 1.0, 1.0], // cyan
+        ];
+    
+        // Define the indices of each triangle for the outer prism
+        let indices = [
+            -0.3, -0.3, -0.3, // inner bottom left back
+            0.3, -0.3, -0.3, // inner bottom right back
+            0, 0.3, -0.3, // inner top back
+            -0.3, -0.3, 0.3, // inner bottom left front
+            0.3, -0.3, 0.3, // inner bottom right front
+            0, 0.3, 0.3, // inner top front
+        ]
+    
+
+        
+        for (let i = 0; i < indices.length; i += 3) {
+            for (let j = 0; j < 3; j++) {
+                let [x1, y1, z1] = outerVertices[indices[i]];
+                let [x2, y2, z2] = outerVertices[indices[i + 1]];
+                let [x3, y3, z3] = outerVertices[indices[i + 2]];
+    
+                let point1 = new Point(x1, y1, z1);
+                let point2 = new Point(x2, y2, z2);
+                let point3 = new Point(x3, y3, z3);
+    
+                let color1 = new Color(...vertexColors[indices[i]]);
+                let color2 = new Color(...vertexColors[indices[i + 1]]);
+                let color3 = new Color(...vertexColors[indices[i + 2]]);
+    
+                this.vertices.push(point1, point2, point3);
+                this.colors.push(color1, color2, color3);
+            }
+        }
+
+
+    }
+
+    initRandomHollow() {
+        let vertices = [
+            // outer horizontal left back
+            [-0.5, 0, -0.5],
+            // outer horizontal right back
+            [0.5, 0, -0.5],
+            // outer horizontal right front
+            [0.5, 0, 0.5],
+            // outer horizontal left front
+            [-0.5, 0, 0.5],
+
+            // inner horizontal left back
+            [-0.4, 0, -0.4],
+            // inner horizontal right back
+            [0.4, 0, -0.4],
+            // inner horizontal right front
+            [0.4, 0, 0.4],
+            // inner horizontal left front
+            [-0.4, 0, 0.4],
+
+            // outer vertical top
+            [0, 0.5, 0],
+            // outer vertical right
+            [0.5, 0, 0],
+            // outer vertical bottom
+            [0, -0.5, 0],
+            // outer vertical left
+            [-0.5, 0, 0],
+
+
+            // inner vertical top
+            [0, 0.4, 0],
+            // inner vertical right
+            [0.4, 0, 0],
+            // inner vertical bottom
+            [0, -0.4, 0],
+            // inner vertical left
+            [-0.4, 0, 0],
+
+            // outer diagonal top left
+            // [-0.5, 0, -0.5],
+
+
+        ]
+
+        let colors = [
+            [0.0, 0.0, 0.0, 1.0], // black
+            [1.0, 0.0, 0.0, 1.0], // red
+            [1.0, 1.0, 0.0, 1.0], // yellow
+            [0.0, 1.0, 0.0, 1.0], // green
+            [0.0, 0.0, 1.0, 1.0], // blue
+            [1.0, 0.0, 1.0, 1.0], // magenta
+            [0.0, 1.0, 1.0, 1.0], // cyan
+            [1.0, 1.0, 1.0, 1.0], // white
+            [0.0, 0.0, 0.0, 1.0], // black
+            [1.0, 0.0, 0.0, 1.0], // red
+            [1.0, 1.0, 0.0, 1.0], // yellow
+            [0.0, 1.0, 0.0, 1.0], // green
+            [0.0, 0.0, 1.0, 1.0], // blue
+            [1.0, 0.0, 1.0, 1.0], // magenta
+            [0.0, 1.0, 1.0, 1.0], // cyan
+            [1.0, 1.0, 1.0, 1.0], // white
+
+        ]
+
+        let indices = [
+            0, 1, 5, 0, 5, 4,
+            1, 2, 6, 1, 6, 5,
+            2, 3, 7, 2, 7, 6,
+            3, 0, 4, 3, 4, 7,
+
+            8, 9, 13, 8, 13, 12,
+            9, 10, 14, 9, 14, 13,
+            10, 11, 15, 10, 15, 14,
+            11, 8, 12, 11, 12, 15,
+
+
+        ]
+
+        for (let i = 0; i < indices.length; i += 3) {
+            for (let j = 0; j < 3; j++) {
+                let [x1, y1, z1] = vertices[indices[i]];
+                let [x2, y2, z2] = vertices[indices[i + 1]];
+                let [x3, y3, z3] = vertices[indices[i + 2]];
+    
+                let point1 = new Point(x1, y1, z1);
+                let point2 = new Point(x2, y2, z2);
+                let point3 = new Point(x3, y3, z3);
+    
+                let color1 = new Color(...colors[indices[i]]);
+                let color2 = new Color(...colors[indices[i + 1]]);
+                let color3 = new Color(...colors[indices[i + 2]]);
+    
+                this.vertices.push(point1, point2, point3);
+                this.colors.push(color1, color2, color3);
+            }
+        }
+
     }
 }
