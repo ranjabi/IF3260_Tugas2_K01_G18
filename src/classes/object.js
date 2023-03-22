86d11adf2e05e4debac9148d4ce9e11e2692aea3
addinnabilal <13520045@std.stei.itb.ac.id>
2023-03-23 00:39:00 +0700

    feat: update random hollow object to have a volume

--- src/classes/object.js
+++ src/classes/object.js
@@ -156,117 +156,124 @@ export default class BaseObject {
             generate_cube_points([point, -point, point], outputPoints, {zdelta: -delta});
     
             return outputPoints
-        }
-
-        
+        }   
     }
 
-    initHollowPrism() {
-        // Define the vertices of the triangles for the outer prism
-        let outerVertices = [       
-            [-0.5, -0.5, -0.5], // bottom left back
-            [0.5, -0.5, -0.5], // bottom right back
-            [0, 0.5, -0.5], // top back
-            [-0.5, -0.5, 0.5], // bottom left front
-            [0.5, -0.5, 0.5], // bottom right front
-            [0, 0.5, 0.5], // top front
-        ];
-    
-        // Define the vertices of the triangles for the inner prism
-        let innerVertices = [        
-            [-0.3, -0.3, -0.3], // inner bottom left back
-            [0.3, -0.3, -0.3], // inner bottom right back
-            [0, 0.3, -0.3], // inner top back
-            [-0.3, -0.3, 0.3], // inner bottom left front
-            [0.3, -0.3, 0.3], // inner bottom right front
-            [0, 0.3, 0.3], // inner top front
-        ];
-    
-        // Define the colors of each vertex
-        let vertexColors = [        
-            [1.0, 0.0, 0.0, 1.0], // red
-            [1.0, 1.0, 0.0, 1.0], // yellow
-            [0.0, 1.0, 0.0, 1.0], // green
-            [0.0, 0.0, 1.0, 1.0], // blue
-            [1.0, 0.0, 1.0, 1.0], // magenta
-            [0.0, 1.0, 1.0, 1.0], // cyan
-        ];
-    
-        // Define the indices of each triangle for the outer prism
-        let indices = [
-            -0.3, -0.3, -0.3, // inner bottom left back
-            0.3, -0.3, -0.3, // inner bottom right back
-            0, 0.3, -0.3, // inner top back
-            -0.3, -0.3, 0.3, // inner bottom left front
-            0.3, -0.3, 0.3, // inner bottom right front
-            0, 0.3, 0.3, // inner top front
-        ]
-    
-
-        
-        for (let i = 0; i < indices.length; i += 3) {
-            for (let j = 0; j < 3; j++) {
-                let [x1, y1, z1] = outerVertices[indices[i]];
-                let [x2, y2, z2] = outerVertices[indices[i + 1]];
-                let [x3, y3, z3] = outerVertices[indices[i + 2]];
-    
-                let point1 = new Point(x1, y1, z1);
-                let point2 = new Point(x2, y2, z2);
-                let point3 = new Point(x3, y3, z3);
-    
-                let color1 = new Color(...vertexColors[indices[i]]);
-                let color2 = new Color(...vertexColors[indices[i + 1]]);
-                let color3 = new Color(...vertexColors[indices[i + 2]]);
-    
-                this.vertices.push(point1, point2, point3);
-                this.colors.push(color1, color2, color3);
-            }
-        }
-
-
-    }
 
     initRandomHollow() {
         let vertices = [
-            // outer horizontal left back
+            // outer horizontal left back top 0
             [-0.5, 0, -0.5],
-            // outer horizontal right back
+            // outer horizontal right back top 1
             [0.5, 0, -0.5],
-            // outer horizontal right front
+            // outer horizontal right front top
             [0.5, 0, 0.5],
             // outer horizontal left front
             [-0.5, 0, 0.5],
 
-            // inner horizontal left back
+            // inner horizontal left back top 4
             [-0.4, 0, -0.4],
-            // inner horizontal right back
+            // inner horizontal right back top
             [0.4, 0, -0.4],
-            // inner horizontal right front
+            // inner horizontal right front top
             [0.4, 0, 0.4],
-            // inner horizontal left front
+            // inner horizontal left front top
             [-0.4, 0, 0.4],
 
-            // outer vertical top
+            // outer horizontal left back bottom 8
+            [-0.5, -0.1, -0.5],
+            // outer horizontal right back bottom
+            [0.5, -0.1, -0.5],
+            // outer horizontal right front bottom
+            [0.5, -0.1, 0.5],
+            // outer horizontal left front bottom
+            [-0.5, -0.1, 0.5],
+
+            // inner horizontal left back bottom 12
+            [-0.4, -0.1, -0.4],
+            // inner horizontal right back bottom
+            [0.4, -0.1, -0.4],
+            // inner horizontal right front bottom
+            [0.4, -0.1, 0.4],
+            // inner horizontal left front bottom
+            [-0.4, -0.1, 0.4],
+
+
+            // outer vertical top front 16
             [0, 0.5, 0],
-            // outer vertical right
+            // outer vertical right front
             [0.5, 0, 0],
-            // outer vertical bottom
+            // outer vertical bottom front
             [0, -0.5, 0],
-            // outer vertical left
+            // outer vertical left front
             [-0.5, 0, 0],
 
 
-            // inner vertical top
+            // inner vertical top front 20
             [0, 0.4, 0],
-            // inner vertical right
+            // inner vertical right top front
             [0.4, 0, 0],
-            // inner vertical bottom
+            // inner vertical bottom bottom front
             [0, -0.4, 0],
-            // inner vertical left
+            // inner vertical left bottom front
             [-0.4, 0, 0],
 
-            // outer diagonal top left
-            // [-0.5, 0, -0.5],
+            // outer vertical top back 24
+            [0, 0.5, -0.1],
+            // outer vertical right back
+            [0.5, 0, -0.1],
+            // outer vertical bottom back
+            [0, -0.5, -0.1],
+            // outer vertical left back
+            [-0.5, 0, -0.1],
+
+            // inner vertical top back 28
+            [0, 0.4, -0.1],
+            // inner vertical right back
+            [0.4, 0, -0.1],
+            // inner vertical bottom back
+            [0, -0.4, -0.1],
+            // inner vertical left back
+            [-0.4, 0, -0.1],
+            
+
+            // outer diagonal top left front 32
+            [-0.5, 0.5, -0.5],
+            // outer diagonal top right front
+            [0.5, 0.5, 0.5],
+            // outer diagonal bottom right front
+            [0.5, -0.5, 0.5],
+            // outer diagonal bottom left front
+            [-0.5, -0.5, -0.5],
+
+            // outer diagonal top left back 36
+            [-0.5, 0.5, -0.6],
+            // outer diagonal top right back
+            [0.5, 0.5, 0.4],
+            // outer diagonal bottom right back
+            [0.5, -0.5, 0.4],
+            // outer diagonal bottom left back
+            [-0.5, -0.5, -0.6],
+
+            // inner diagonal top left front 40
+            [-0.4, 0.4, -0.4],
+            // inner diagonal top right front
+            [0.4, 0.4, 0.4],
+            // inner diagonal bottom right front
+            [0.4, -0.4, 0.4],
+            // inner diagonal bottom left front
+            [-0.4, -0.4, -0.4],
+
+            // inner diagonal top left back 44
+            [-0.4, 0.4, -0.5],
+            // inner diagonal top right back
+            [0.4, 0.4, 0.3],
+            // inner diagonal bottom right back
+            [0.4, -0.4, 0.3],
+            // inner diagonal bottom left back
+            [-0.4, -0.4, -0.5],
+
+
 
 
         ]
@@ -278,17 +285,6 @@ export default class BaseObject {
             [0.0, 1.0, 0.0, 1.0], // green
             [0.0, 0.0, 1.0, 1.0], // blue
             [1.0, 0.0, 1.0, 1.0], // magenta
-            [0.0, 1.0, 1.0, 1.0], // cyan
-            [1.0, 1.0, 1.0, 1.0], // white
-            [0.0, 0.0, 0.0, 1.0], // black
-            [1.0, 0.0, 0.0, 1.0], // red
-            [1.0, 1.0, 0.0, 1.0], // yellow
-            [0.0, 1.0, 0.0, 1.0], // green
-            [0.0, 0.0, 1.0, 1.0], // blue
-            [1.0, 0.0, 1.0, 1.0], // magenta
-            [0.0, 1.0, 1.0, 1.0], // cyan
-            [1.0, 1.0, 1.0, 1.0], // white
-
         ]
 
         let indices = [
@@ -296,13 +292,40 @@ export default class BaseObject {
             1, 2, 6, 1, 6, 5,
             2, 3, 7, 2, 7, 6,
             3, 0, 4, 3, 4, 7,
-
+            0, 8, 9, 0, 9, 1,
+            1, 9, 10, 1, 10, 2,
+            2, 10, 11, 2, 11, 3,
+            3, 11, 8, 3, 8, 0,
             8, 9, 13, 8, 13, 12,
             9, 10, 14, 9, 14, 13,
             10, 11, 15, 10, 15, 14,
             11, 8, 12, 11, 12, 15,
 
+            16, 17, 21, 16, 21, 20,
+            17, 18, 22, 17, 22, 21,
+            18, 19, 23, 18, 23, 22,
+            19, 16, 20, 19, 20, 23,
+            16, 24, 25, 16, 25, 17,
+            17, 25, 26, 17, 26, 18,
+            18, 26, 27, 18, 27, 19,
+            19, 27, 24, 19, 24, 16,
+            24, 25, 29, 24, 29, 28,
+            25, 26, 30, 25, 30, 29,
+            26, 27, 31, 26, 31, 30,
+            27, 24, 28, 27, 28, 31,
 
+            32, 33, 37, 32, 37, 36,
+            33, 34, 38, 33, 38, 37,
+            34, 35, 39, 34, 39, 38,
+            35, 32, 36, 35, 36, 39,
+            32, 40, 41, 32, 41, 33,
+            33, 41, 42, 33, 42, 34,
+            34, 42, 43, 34, 43, 35,
+            35, 43, 40, 35, 40, 32,
+            40, 41, 45, 40, 45, 44,
+            41, 42, 46, 41, 46, 45,
+            42, 43, 47, 42, 47, 46,
+            43, 40, 44, 43, 44, 47,
         ]
 
         for (let i = 0; i < indices.length; i += 3) {
@@ -315,9 +338,9 @@ export default class BaseObject {
                 let point2 = new Point(x2, y2, z2);
                 let point3 = new Point(x3, y3, z3);
     
-                let color1 = new Color(...colors[indices[i]]);
-                let color2 = new Color(...colors[indices[i + 1]]);
-                let color3 = new Color(...colors[indices[i + 2]]);
+                let color1 = new Color(...colors[indices[j]]);
+                let color2 = new Color(...colors[indices[j + 1]]);
+                let color3 = new Color(...colors[indices[j + 2]]);
     
                 this.vertices.push(point1, point2, point3);
                 this.colors.push(color1, color2, color3);
